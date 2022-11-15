294785f9117e3cbcfbd05812a34818ecf2cd0012
cowbra <hugosanchez1303@gmail.com>
2022-11-15 16:16:48 +0100

    feat(5): Hello World

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const locationController = require('./locations/locations.controller')
+const application_listener = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+application_listener.use(locationController)
+
+
+async function main(){
+    const result = await mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Connected")});
+    application_listener.listen(port, () => {
+        console.log(`API listening on port ${port}`)
+    })
+}
+main()
+
+
+
+
+
+
+application_listener.get('', (req,res) => {    res.send("Hello World")})
